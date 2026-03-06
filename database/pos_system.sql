40cca79b6fdc5e4c651367015e222ace0b201410
Tanzeel Khan <fattia923@gmail.com>
2026-03-06 08:06:49 -0800

    Update pos_system.sql

--- database/pos_system.sql
+++ database/pos_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2026 at 09:46 AM
+-- Generation Time: Mar 06, 2026 at 05:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,7 +44,7 @@ CREATE TABLE `admins` (
 INSERT INTO `admins` (`id`, `name`, `email`, `password`, `phone`, `is_ban`, `created_at`) VALUES
 (1, 'Hashir khan', 'hashir@gmail.com', '$2y$10$TndabLKihQ6ho3tHV/Do3.n1wK5nYYCkF/jcLjKggT1FPCo4I85yO', '03039145534', 1, '2026-02-11'),
 (2, 'asad', 'asad@gmail.com', '$2y$10$RVny8QGjWdBHweONjyDGVuWp/HyIpfnGoTlfi.7iSZf5t6zHvpXZ2', '03075219319', 1, '2026-02-11'),
-(3, 'ahsan', 'ahsan@gmail.com', '$2y$10$RFs3ASNYyRq1amZkyN.UDe6buT7JBxTdGpmZXbmJiuli998cW/DFu', '03421796634', 1, '2026-02-11');
+(3, 'ahsan', 'ahsan@gmail.com', '$2y$10$vafdnh0Z7xfdESOhQfXyC.RiCyMoWMgrFE4x6lFiRYxB7qY184VYK', '03421796634', 1, '2026-02-11');
 
 -- --------------------------------------------------------
 
@@ -72,6 +72,38 @@ CREATE TABLE `customers` (
   `phone` varchar(255) DEFAULT NULL,
   `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0=visible,1=hidden',
   `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `tracking_no` varchar(100) NOT NULL,
+  `invoice_no` varchar(100) NOT NULL,
+  `total_amount` varchar(100) NOT NULL,
+  `order_date` date NOT NULL,
+  `order_status` varchar(100) DEFAULT NULL,
+  `payment_mode` varchar(100) NOT NULL COMMENT 'cash,online',
+  `order_placed_by_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `quantity` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -115,6 +147,18 @@ ALTER TABLE `customers`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -140,6 +184,18 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
